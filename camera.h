ce95efccd08c1627c756de04c960bf6469fc9eb3
Helens-dot <374961015@qq.com>
2023-10-08 10:11:07 +0800

    ray tracing in one weekend

--- camera.h
+++ camera.h
@@ -7,14 +7,21 @@
 class camera
 {
 public:
-	camera(int _image_width, double ratio, int _samples, int depth):image_width(_image_width), aspect_ratio(ratio), samples_per_pixel(_samples), max_depth(depth)
+	point3 lookfrom = point3(0, 0, -1);  // Point camera is looking from
+	point3 lookat = point3(0, 0, 0);   // Point camera is looking at
+	vec3   vup = vec3(0, 1, 0);     // Camera-relative "up" direction
+	double defocus_angle = 0;  // Variation angle of rays through each pixel
+	double focus_dist = 10;    // Distance from camera lookfrom point to plane of perfect focus
+public:
+	camera(int _image_width, double ratio, int _samples, int depth, double _vfov):image_width(_image_width), aspect_ratio(ratio), 
+			samples_per_pixel(_samples), max_depth(depth), vfov(_vfov)
 	{
-		initialize();
+		
 	}
 
 	void render(const hittable& world)
 	{
-
+		initialize();
 		std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 		for (int j = 0; j < image_height; j++) {
@@ -40,22 +47,31 @@ private:
 		image_height = static_cast<int>(image_width / aspect_ratio);
 		image_height = image_height < 1 ? 1 : image_height;
 
-		camera_center = point3(0, 0, 0);
+		camera_center = lookfrom;
 
-		auto focal_length = 1.0;
-		auto viewport_height = 2.0;
+		auto theta = degrees_to_radians(vfov);
+		auto h = tan(theta / 2);
+		auto viewport_height = 2 * h * focus_dist;
 		auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup, w));
+		v = cross(w, u);
 
-		auto viewport_u = vec3(viewport_width, 0, 0);
-		auto viewport_v = vec3(0, -viewport_height, 0);
+		auto viewport_u = viewport_width * u;
+		auto viewport_v = viewport_height*-v;
 
 		pixel_delta_u = viewport_u / image_width;
 		pixel_delta_v = viewport_v / image_height;
 
-		auto viewport_upper_left = camera_center - viewport_u / 2 - viewport_v / 2 - vec3(0, 0, focal_length);
+		auto viewport_upper_left = camera_center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
 
 		pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+		// Calculate the camera defocus disk basis vectors.
+		auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+		defocus_disk_u = u * defocus_radius;
+		defocus_disk_v = v * defocus_radius;
 	}
 
 	color ray_color(const ray& r, int depth, const hittable& world) {
@@ -103,11 +119,17 @@ private:
 
 		auto pixel_sample = pixel_center + pixel_sample_square();
 
-		auto ray_origin = camera_center;
+		auto ray_origin = (defocus_angle <= 0) ? camera_center : defocus_disk_sample();
 		auto ray_direction = pixel_sample - ray_origin;
 
 		ray r(ray_origin, ray_direction);
 		return r;
+	}
+
+	point3 defocus_disk_sample() const {
+		// Returns a random point in the camera defocus disk.
+		auto p = random_in_unit_disk();
+		return camera_center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
 	}
 
 	vec3 pixel_sample_square() const	//从像素中间进行随机采样
@@ -127,4 +149,10 @@ private:
 	vec3 pixel_delta_v;		//offset to pixel to below
 	int samples_per_pixel;	//count of random samples of each pixel
 	int max_depth;			//max num of ray bounces into scene
+	double vfov;
+
+	vec3 u, v, w;
+
+	vec3   defocus_disk_u;  // Defocus disk horizontal radius
+	vec3   defocus_disk_v;  // Defocus disk vertical radius
 };
